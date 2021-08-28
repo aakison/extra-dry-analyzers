bb375584a130491a685ad327c2e0cb0778cc6c39
Adrian Akison <adrian@akison.com>
2021-08-28 18:00:58 +1000

    Refactor method for determining visibility, unsure of null exception thrown in target project

---
+++ Blazor.ExtraDry.Analyzers/Blazor.ExtraDry.Analyzers/Visibility.cs
@@ -0,0 +1,14 @@
+﻿namespace Blazor.ExtraDry.Analyzers {
+    public enum Visibility {
+
+        Public,
+
+        Private,
+
+        Protected,
+
+        Internal,
+
+        // ProtectedInternal - not supported...
+    }
+}
