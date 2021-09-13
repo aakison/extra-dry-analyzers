183dc7ae22a38aa5cfb0e99918d674f0922857a8
Adrian Akison <adrian@akison.com>
2021-09-13 21:17:36 +1000

    Add check on methods that need security attributes

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1105_HttpVerbsShouldHaveExplicitSecurity.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.HttpVerbsShouldHaveExplicitSecurity>;
+
+namespace ExtraDry.Analyzers.Test
+{
+    public class HttpVerbsShouldHaveExplicitSecurity {
+
+        [Theory]
+        [InlineData("HttpPatch", "AllowAnonymous")] // Set up alright
+        [InlineData("HttpPatch", "Authorize")]
+        [InlineData("HttpPut", "AllowAnonymous")]
+        [InlineData("HttpPut", "Authorize")]
+        [InlineData("HttpDelete", "AllowAnonymous")]
+        [InlineData("HttpDelete", "Authorize")]
+        [InlineData("HttpPost", "AllowAnonymous")]
+        [InlineData("HttpPost", "Authorize")]
+        [InlineData("HttpGet", "AllowAnonymous")]
+        [InlineData("HttpGet", "Authorize")]
+        [InlineData("AllowAnonymous", "Route")] // No verb
+        [InlineData("Authorize", "Route")]
+        public async Task AllGood_NoDiagnostic(string verb, string security)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+public class SampleController {{
+    [{verb}]
+    [{security}]
+    public void Method(int id) {{}}
+}}
+");
+        }
+
+        [Theory]
+        [InlineData("HttpPatch")]
+        [InlineData("HttpPut")]
+        [InlineData("HttpDelete")]
+        [InlineData("HttpPost")]
+        [InlineData("HttpGet")]
+        public async Task MissingSecurity_Diagnostic(string verb)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+public class SampleController {{
+    [{verb}]
+    public void [|Method|](int id) {{}}
+}}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
