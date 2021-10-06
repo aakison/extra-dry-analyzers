3149dd4a3eed08505e3efe22048d0265569ac5f5
Adrian Akison <adrian@akison.com>
2021-10-07 09:36:56 +1000

    1204 consumes on method

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1204_ConsumesAttributeNotOnClassTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.ConsumesAttributeNotOnClass>;
+
+namespace ExtraDry.Analyzers.Test
+{
+    public class ConsumesAttributeNotOnClassTests {
+
+        [Fact]
+        public async Task AllGood_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Consumes(""application/json"")]
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task NotOnApiController_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+[[|Consumes(""application/json"")|]]
+public class SampleController {
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task NotOnAnyClass_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[[|Consumes(""application/json"")|]]
+public class SampleController {
+    public void Method(int id) {}
+}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1204_ConsumesAttributeNotOnClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ConsumesAttributeNotOnClass : DryDiagnosticNodeAnalyzer {
+
+        public ConsumesAttributeNotOnClass() : base(
+            SyntaxKind.ClassDeclaration,
+            1204,
+            DryAnalyzerCategory.OpenApiDocs,
+            DiagnosticSeverity.Warning,
+            "ConsumesAttribute should only apply to methods.",
+            "ConsumesAttribute on class {0} should be removed and placed on the appropriate methods.",
+            "The Consumes attribute indicates to API consumers what the type of the request payload should be.  Placing on a class will indicate a payload on all methods, including those that shouldn't produce anything (e.g. List, Retrieve)."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var _class = (ClassDeclarationSyntax)context.Node;
+            var hasProducesAttribute = HasAnyAttribute(context, _class, out var producesAttribute, "Consumes");
+            if(!hasProducesAttribute) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, producesAttribute.GetLocation(), _class.Identifier.ValueText));
+        }
+
+    }
+}
