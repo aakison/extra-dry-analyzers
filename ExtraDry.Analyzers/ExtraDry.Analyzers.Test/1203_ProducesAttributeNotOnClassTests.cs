bb987a95e5cbabb63f27b74b9aea2cb4c753c1fe
Adrian Akison <adrian@akison.com>
2021-10-07 09:13:51 +1000

    1203 produces attribute not at class level

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1203_ProducesAttributeNotOnClassTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.ProducesAttributeNotOnClass>;
+
+namespace ExtraDry.Analyzers.Test
+{
+    public class ProducesAttributeNotOnClassTests {
+
+        [Fact]
+        public async Task AllGood_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Consumes(""application/json"")]
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task NotOnApiController_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+[[|Produces(""application/json"")|]]
+public class SampleController {
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task NotOnAnyClass_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[[|Produces(""application/json"")|]]
+public class SampleController {
+    public void Method(int id) {}
+}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1203_ProducesAttributeNotOnClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ProducesAttributeNotOnClass : DryDiagnosticNodeAnalyzer {
+
+        public ProducesAttributeNotOnClass() : base(
+            SyntaxKind.ClassDeclaration,
+            1203,
+            DryAnalyzerCategory.OpenApiDocs,
+            DiagnosticSeverity.Warning,
+            "ProducesAttribute should only apply to methods.",
+            "ProducesAttribute on class {0} should be removed and placed on the appropriate methods.",
+            "The Produces attribute indicates to API consumers what the type of the response payload will be.  Placing on a class will indicate a response on all methods, including those that shouldn't produce anything (e.g. PUT, DELETE)."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var _class = (ClassDeclarationSyntax)context.Node;
+            var hasProducesAttribute = HasAnyAttribute(context, _class, out var producesAttribute, "Produces");
+            if(!hasProducesAttribute) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, producesAttribute.GetLocation(), _class.Identifier.ValueText));
+        }
+
+    }
+}
