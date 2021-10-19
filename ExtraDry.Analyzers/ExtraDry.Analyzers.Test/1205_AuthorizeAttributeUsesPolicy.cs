8fdce5f55010d5de9c06f3c92045d96bd904a805
Adrian Akison <adrian@akison.com>
2021-10-20 08:48:09 +1000

    Rule to ensure that authorize attribute is logically correct

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1205_AuthorizeAttributeUsesPolicy.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.AuthorizeAttributeUsesPolicy>;
+
+namespace ExtraDry.Analyzers.Test
+{
+    public class AuthorizeAttributeUsesPolicy {
+
+        [Fact]
+        public async Task NoAttribute_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    public void Method() {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesConstructor_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize(""policy-name"")]
+    public void Method() {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesProperty_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize(Policy = ""role-name"")]
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task NoConstructorNoProperties_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [[|Authorize|]]
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesRoles_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [[|Authorize(Roles = ""role-name"")|]]
+    public void Method(int id) {}
+}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1205_AuthorizeAttributeUsesPolicy.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AuthorizeAttributeUsesPolicy : DryDiagnosticNodeAnalyzer {
+
+        public AuthorizeAttributeUsesPolicy() : base(
+            SyntaxKind.MethodDeclaration,
+            1205,
+            DryAnalyzerCategory.Security,
+            DiagnosticSeverity.Warning,
+            "AuthorizeAttribute should use policies.",
+            "AuthorizeAttribute on method {0} should use a policy.",
+            "The Authorize attribute controls access to the method/action and should have a descriptive and extensible security mechanism.  Don't rely solely on authentication for access, consider at least a basic 'stakeholder' type policy for authenticated users.  Also, do not rely on Roles which are less functional and not extensible."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var _method = (MethodDeclarationSyntax)context.Node;
+            var hasAuthorizeAttribute = HasAnyAttribute(context, _method, out var authorizeAttribute, "Authorize");
+            if(!hasAuthorizeAttribute) {
+                return;
+            }
+            //var roleNamed = NamedArgument(authorizeAttribute, "Roles");
+            var policyPositional = FirstArgument(authorizeAttribute);
+            var policyNamed = NamedArgument(authorizeAttribute, "Policy");
+            if(policyNamed != null || policyPositional != null) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, authorizeAttribute.GetLocation(), _method.Identifier.ValueText));
+        }
+
+    }
+}
