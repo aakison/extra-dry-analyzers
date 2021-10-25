717c56ee5d90f1b1bb5392b975afeaeb55b24f46
Adrian Akison <adrian@akison.com>
2021-10-26 09:10:54 +1000

    Add check against public static methods on API Controllers

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1009_ApiControllerShouldNotHavePublicStatic.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.ApiControllerShouldNotHavePublicStatic>;
+
+namespace ExtraDry.Analyzers.Test
+{
+
+    public class ApiControllerShouldNotHavePublicStaticTests {
+
+        [Fact]
+        public async Task StaticIsPrivate_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    private static void Retrieve(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task PublicIsInstance_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    public void Retrieve(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task NotApiController_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+public class SampleController {
+    public static void Retrieve(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task PublicAndStatic_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    public static void [|Retrieve|](int id) {}
+}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
