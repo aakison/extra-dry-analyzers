3675f8be43cecb178a8736221c0342cfa9180226
Adrian Akison <adrian@akison.com>
2021-11-13 16:44:18 +1000

    Create rule that SkipStatusCodePages attribute should not be on method

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1112_HttpVerbsShouldNotHaveSkipStatusCodePagesTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.HttpVerbsShouldNotHaveSkipStatusCodePages>;
+
+namespace ExtraDry.Analyzers.Test
+{
+    public class HttpVerbsShouldNotHaveSkipStatusCodePagesTests {
+
+        [Theory]
+        [InlineData("HttpPatch")]
+        [InlineData("HttpPut")]
+        [InlineData("HttpDelete")]
+        [InlineData("HttpPost")]
+        [InlineData("HttpGet")]
+        public async Task AllGood_NoDiagnostic(string verb)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+[SkipStatusCodePages]
+public class SampleController {{
+    [{verb}(""/route"")]
+    public void Retrieve(int id) {{}}
+}}
+");
+        }
+
+        [Theory]
+        [InlineData("HttpPatch")]
+        [InlineData("HttpPut")]
+        [InlineData("HttpDelete")]
+        [InlineData("HttpPost")]
+        [InlineData("HttpGet")]
+        public async Task HasSkipAtMethod_Diagnostic(string verb)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+public class SampleController : Controller {{
+    [{verb}]
+    [[|SkipStatusCodePages|]]
+    public void Retrieve(int id) {{}}
+}}
+");
+        }
+
+        [Theory]
+        [InlineData("HttpPatch")]
+        [InlineData("HttpPut")]
+        [InlineData("HttpDelete")]
+        [InlineData("HttpPost")]
+        [InlineData("HttpGet")]
+        public async Task HasSkipAtMethodCombined_Diagnostic(string verb)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+public class SampleController : Controller {{
+    [{verb}, [|SkipStatusCodePages|]]
+    public void Retrieve(int id) {{}}
+}}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1112_HttpVerbsShouldNotHaveSkipStatusCodePages.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class HttpVerbsShouldNotHaveSkipStatusCodePages : DryDiagnosticNodeAnalyzer {
+
+        public HttpVerbsShouldNotHaveSkipStatusCodePages() : base(
+            SyntaxKind.MethodDeclaration,
+            1112,
+            DryAnalyzerCategory.Usage,
+            DiagnosticSeverity.Warning,
+            "Methods with Http Verbs should not have SkipStatusCodePages attribute",
+            "Method `{0}` should not have SkipStatusCodePages attribute",
+            "The SkipStatusCodePages attribute can be on either the class or the method.  However, for API controllers, it should always be applied and the attribute should be applied at the class level.  Methods within an API controller should not mix and match this attribute."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var method = (MethodDeclarationSyntax)context.Node;
+            var hasVerbAttribute = HasAnyAttribute(context, method, out var _, "HttpGet", "HttpPost", "HttpPut", "HttpPatch", "HttpDelete");
+            if(!hasVerbAttribute) {
+                return;
+            }
+            var hasSkipAttribute = HasAttribute(context, method, "SkipStatusCodePages", out var skipAttribute);
+            if(!hasSkipAttribute) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, skipAttribute.GetLocation(), skipAttribute.Name.ToFullString(), method.Identifier.ValueText));
+        }
+
+    }
+}
