97b4f93efbfbab1206e9ae5675bf906830e8eaa4
Adrian Akison <adrian@akison.com>
2022-07-15 10:14:46 +1000

    DRY1207

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1207_AuthorizeAttributePolicyNotLiteralTests.cs
@@ -0,0 +1,146 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.AuthorizeAttributePolicyNotLiteral>;
+
+namespace ExtraDry.Analyzers.Test
+{
+    public class AuthorizeAttributePolicyNotLiteralTests {
+
+        [Fact]
+        public async Task NoAttribute_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    public void Method() {}
+}
+");
+        }
+
+        [Fact]
+        public async Task PolicyNotSpecified_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize]
+    public void Method() {}
+}
+");
+        }
+
+        [Fact]
+        public async Task AllowAnonymous_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [AllowAnonymous]
+    public void Method() {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesConst_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize(PolicyName)]
+    public void Method() {}
+    const string PolicyName = ""policy-name"";
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesPositionalConst_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize(Policy = PolicyName)]
+    public void Method() {}
+    const string PolicyName = ""policy-name"";
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesNameof_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize(nameof(SampleController))]
+    public void Method() {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesPositionalNameof_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize(Policy = nameof(SampleController))]
+    public void Method() {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesRoles_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Authorize(Roles = ""role-name"")]
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesPositionalPolicy_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [[|Authorize(""policy-name"")|]]
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesPositionalAndRoles_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [[|Authorize(""policy-name"", Roles = ""role-name"")|]]
+    public void Method(int id) {}
+}
+");
+        }
+
+        [Fact]
+        public async Task UsesBothProperties_Diagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [[|Authorize(Policy = ""policy-name"", Roles = ""role-name"")|]]
+    public void Method(int id) {}
+}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1207_AuthorizeAttributePolicyNotLiteral.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AuthorizeAttributePolicyNotLiteral : DryDiagnosticNodeAnalyzer {
+
+        public AuthorizeAttributePolicyNotLiteral() : base(
+            SyntaxKind.MethodDeclaration,
+            1207,
+            DryAnalyzerCategory.Security,
+            DiagnosticSeverity.Warning,
+            "AuthorizeAttribute policies should use shared constant and not string literals.",
+            "AuthorizeAttribute on method {0} should use a policy.",
+            "The Authorize attribute policies are keyed by shared names that are used to lookup requirements.  The use of string literals as 'magic strings' to key these items is prone to error when adding and editing code.  Instead, use a compile-time checked substitute such as a `const` value or using `nameof` with an associated requirement."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var _method = (MethodDeclarationSyntax)context.Node;
+            var hasAuthorizeAttribute = HasAnyAttribute(context, _method, out var authorizeAttribute, "Authorize");
+            if(!hasAuthorizeAttribute) {
+                return;
+            }
+            var policyPositional = FirstArgument(authorizeAttribute);
+            var validPositional = policyPositional == null ? true : policyPositional?.Kind() != SyntaxKind.StringLiteralExpression;
+            var policyNamed = NamedArgument(authorizeAttribute, "Policy");
+            var validNamed = policyNamed == null ? true : policyNamed?.Kind() != SyntaxKind.StringLiteralExpression;
+            if(validPositional && validNamed) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, authorizeAttribute.GetLocation(), _method.Identifier.ValueText));
+        }
+
+    }
+}
