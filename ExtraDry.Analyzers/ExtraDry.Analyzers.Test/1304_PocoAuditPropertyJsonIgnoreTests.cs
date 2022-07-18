82f149c97f8911e9a65a86241c09c58c4d1f881f
Adrian Akison <adrian@akison.com>
2022-07-18 19:49:14 +1000

    DRY1304

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1304_PocoAuditPropertyJsonIgnoreTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.PocoAuditPropertyJsonIgnore>;
+
+namespace ExtraDry.Analyzers.Test
+{
+    public class PocoAuditPropertyJsonIgnoreTest {
+
+        [Theory]
+        [InlineData("OkName")]
+        [InlineData("Audit")]
+        [InlineData("AuditRecord")]
+        [InlineData("Version")]
+        [InlineData("VersionInfo")]
+        public async Task NoLeakagePrivate_NoDiagnostic(string name)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+public class SampleEntity {{
+    private string {name} {{ get; set; }}
+}}
+");
+        }
+
+        [Theory]
+        [InlineData("", "OkName")]
+        [InlineData("[JsonIgnore]", "Audit")]
+        [InlineData("[JsonIgnore]", "AuditRecord")]
+        [InlineData("[JsonIgnore]", "Version")]
+        [InlineData("[JsonIgnore]", "VersionInfo")]
+        public async Task NoLeakageIgnored_NoDiagnostic(string attribute, string name)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+public class SampleEntity {{
+    {attribute}
+    public string {name} {{ get; set; }}
+}}
+");
+        }
+
+        [Theory]
+        [InlineData("Audit")]
+        [InlineData("AuditRecord")]
+        [InlineData("Version")]
+        [InlineData("VersionInfo")]
+        public async Task PossibleLeakage_Diagnostic(string name)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+public class SampleEntity {{
+    public string [|{name}|] {{ get; set; }}
+}}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1304_PocoAuditPropertyJsonIgnore.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Linq;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class PocoAuditPropertyJsonIgnore : DryDiagnosticNodeAnalyzer {
+
+        public PocoAuditPropertyJsonIgnore() : base(
+            SyntaxKind.PropertyDeclaration,
+            1304,
+            DryAnalyzerCategory.Security,
+            DiagnosticSeverity.Warning,
+            "Properties that might leak PID should be JsonIgnore.",
+            "The property '{0}' should be decorated with [JsonIgnore].",
+            "Properties that record information about versions and auditing might inadvertantly contain personally identifable data (PID).  This information is typically considered protected and care should be taken, especially in areas where it isn't normally considered, such as properties used for 'audit'ing and 'version'ing."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var property = (PropertyDeclarationSyntax)context.Node;
+            var isIgnored = HasAttribute(context, property, "JsonIgnore", out var _);
+            if(isIgnored) {
+                return;
+            }
+            var isPublic = HasVisibility(property, Visibility.Public);
+            if(!isPublic) {
+                return;
+            }
+            var invalidStems = new string[] { "audit", "version" };
+            var propertyName = property.Identifier.ValueText.ToLowerInvariant();
+            var hasInvalidStem = invalidStems.Any(e => propertyName.Contains(e));
+            if(!hasInvalidStem) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, property.Identifier.GetLocation(), property.Identifier.ValueText));
+        }
+
+    }
+}
