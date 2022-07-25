7cdd1b4e7bd473dc40cf2664a477bf84a1d39a85
Adrian Akison <adrian@akison.com>
2022-07-25 13:48:36 +1000

    Added DRY1413

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1413_MvcControllerPropertyShouldNotInjectDbContextTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.MvcControllerPropertyShouldNotInjectDdContext>;
+
+namespace ExtraDry.Analyzers.Test
+{
+
+    public class MvcControllerPropertyShouldNotInjectDbContextTests {
+
+        [Fact]
+        public async Task CorrectUsageNoInject()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+public class SampleController : Controller {
+    public string JustAProperty { get; set; }
+}
+");
+        }
+
+        [Fact]
+        public async Task CorrectUsageInject()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+public class SampleController : Controller {
+    [Inject]
+    public string JustAProperty { get; set; }
+}
+");
+        }
+
+        [Fact]
+        public async Task IncorrectButForMvcRule_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [Inject]
+    public DbContext Database { get; set; }
+}
+");
+        }
+
+        [Theory]
+        [InlineData("DbContext")]
+        [InlineData("SampleContext")]
+        public async Task DependencyOnDbContext_Diagnostic(string className)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+public class SampleController : Controller {{
+    [Inject]
+    public {className} [|SampleProperty|] {{ get; set; }}
+}}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
