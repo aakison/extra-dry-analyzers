fba24a27de2c57aefd16596bfa9ea8910e1533a9
Adrian Akison <adrian@akison.com>
2022-10-10 10:45:55 +1000

    DRY1015 Add FilterQuery naming check

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1000_ApiControllers/1015_ApiControllerQueriesNamedQueryTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.ApiControllerQueriesNamedQuery>;
+
+namespace ExtraDry.Analyzers.Test
+{
+
+    public class ApiControllerQueriesNamedQueryTests {
+
+        [Fact]
+        public async Task NoQuery_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [HttpGet(""/api/test-items"")]
+    public List<object> TestMethod() {
+        return new List<object>();
+    }
+}
+");
+        }
+
+        [Fact]
+        public async Task NotFilterQuery_NoDiagnostic()
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @"
+[ApiController]
+public class SampleController {
+    [HttpGet(""/api/test-items"")]
+    public List<object> TestMethod(string filter) {
+        return new List<object>();
+    }
+}
+");
+        }
+
+        [Theory]
+        [InlineData("FilterQuery")]
+        [InlineData("PageQuery")]
+        public async Task CorrectUsage_NoDiagnostic(string className)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @$"
+[ApiController]
+public class SampleController {{
+    [HttpGet(""/api/test-items"")]
+    public List<object> TestMethod({className} query) {{
+        return new List<object>();
+    }}
+}}
+");
+        }
+
+        [Theory]
+        [InlineData("FilterQuery", "filter")]
+        [InlineData("PageQuery", "pager")]
+        public async Task NoFromQuery_Diagnostic(string className, string badName)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(stubs + @$"
+[ApiController]
+public class SampleController {{
+    [HttpGet(""/api/test-items"")]
+    public List<object> TestMethod([|{className} {badName}|]) {{
+        return new List<object>();
+    }}
+}}
+");
+        }
+
+        public string stubs = TestHelpers.Stubs;
+
+    }
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1000_ApiControllers/1015_ApiControllerQueriesNamedQuery.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class ApiControllerQueriesNamedQuery : DryDiagnosticNodeAnalyzer {
+
+        public ApiControllerQueriesNamedQuery() : base(
+            SyntaxKind.MethodDeclaration,
+            1015,
+            DryAnalyzerCategory.Usage,
+            DiagnosticSeverity.Warning,
+            "FilterQuery and PageQuery parameters should be named 'query'.",
+            "Method '{0}' should explicity declare parameter with name 'query'.",
+            "The query mechanism in ExtraDry can be used for either filter queries or page queries, and both of these objects have properties that can easily be confused with names other than 'query'.  For example, naming a filter query `filter` implies a property called `filter.filter`.  This can lead to routes not aligning.  Further, the ability to upgrade from a FilterQuery to a PageQuery is possible but only if the parameter name is not changed."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var method = (MethodDeclarationSyntax)context.Node;
+            var filterParameter = FirstTypeParameter(context, method, "FilterQuery"); // PageQuery included as it inherits FilterQuery.
+            if(filterParameter == null) {
+                return;
+            }
+            if(filterParameter.Identifier.ValueText == "query") {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, filterParameter.GetLocation(), method.Identifier.ValueText));
+        }
+
+    }
+}
