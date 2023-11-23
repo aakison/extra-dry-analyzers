e6747b730b11001287c0ca29dec0ff7a2f521258
Adrian Akison <adrian@akison.com>
2023-11-23 10:45:17 +1000

    Add DRY1114 - ensure create methods return ResourceReference

---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers.Test/1100_ControllerVerbs/1114_HttpCreateVerbsShouldReturnResourceReferenceTests.cs
@@ -0,0 +1,84 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+using VerifyCS = ExtraDry.Analyzers.Test.CSharpAnalyzerVerifier<
+    ExtraDry.Analyzers.HttpCreateVerbsShouldReturnResourceReference>;
+
+namespace ExtraDry.Analyzers.Test;
+
+
+public class HttpCreateVerbsShouldReturnResourceReferenceTests {
+
+    [Fact]
+    public async Task AllGood_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+public class SampleController {{
+    [HttpPost]
+    [Produces(""application/json"")]
+    public ResourceReference Create(int id) {{
+        throw new NotImplementedException();
+    }}
+}}
+");
+    }
+
+    [Fact]
+    public async Task NoProducesNotThisRule_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+public class SampleController : Controller {{
+    [HttpPost]
+    public ResourceReference Method(int id) {{
+        throw new NotImplementedException();
+    }}
+}}
+");
+    }
+
+
+    [Fact]
+    public async Task NotApiController_NoDiagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+public class SampleController : Controller {{
+    [HttpPost]
+    public void Method(int id) {{}}
+}}
+");
+    }
+
+    [Theory]
+    [InlineData("HttpPatch")]
+    [InlineData("HttpPut")]
+    [InlineData("HttpDelete")]
+    public async Task NotACreateMethod_NoDiagnostic(string verb)
+    {
+        await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+public class SampleController {{
+    [{verb}]
+    [Produces("""")]
+    public void Method(int id) {{}}
+}}
+");
+    }
+
+    [Fact]
+    public async Task InvalidReturnType_Diagnostic()
+    {
+        await VerifyCS.VerifyAnalyzerAsync(stubs + $@"
+[ApiController]
+[Produces("""")]
+public class SampleController {{
+    [HttpPost]
+    public void [|Method|](int id) {{}}
+}}
+");
+    }
+
+    public string stubs = TestHelpers.Stubs;
+
+
+}
---
+++ ExtraDry.Analyzers/ExtraDry.Analyzers/1100_ControllerVerbs/1114_HttpCreateVerbsShouldReturnResourceReference.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace ExtraDry.Analyzers {
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class HttpCreateVerbsShouldReturnResourceReference : DryDiagnosticNodeAnalyzer {
+
+        public HttpCreateVerbsShouldReturnResourceReference() : base(
+            SyntaxKind.MethodDeclaration,
+            1114,
+            DryAnalyzerCategory.ApiUsage,
+            DiagnosticSeverity.Info,
+            "HttpPost actions should Produces ResourceReference output.",
+            "Method `{0}` should declare that it produces a response body of type ResourceReference.",
+            "The Produces attribute indicates to API consumers what the type of the response payload will be.  Create actions should have a response body that indicates or confirms the entity keys, UUID and Slug."
+            )
+        { }
+
+        public override void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var method = (MethodDeclarationSyntax)context.Node;
+            var hasPostAttribute = HasAnyAttribute(context, method, out var _, "HttpPost");
+            if(!hasPostAttribute) {
+                return;
+            }
+            var _class = ClassForMember(method);
+            if(_class == null) {
+                return;
+            }
+            var hasApiController = HasAttribute(context, _class, "ApiController", out var _);
+            if(!hasApiController) {
+                return;
+            }
+            var idPayloadReturn = AnyReturnMatches(method, out var _, "ResourceReference");
+            if(idPayloadReturn) {
+                return;
+            }
+            context.ReportDiagnostic(Diagnostic.Create(Rule, method.Identifier.GetLocation(), method.Identifier.ValueText));
+        }
+
+    }
+}
